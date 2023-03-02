b616d15e1f16c34716cc2e733769893988f77a43
Jacky <jchoi53@mail.ccsf.edu>
2023-03-02 13:26:54 -0800

    030223

--- March2023/8kyus/sumofpositives.js
+++ March2023/8kyus/sumofpositives.js
@@ -6,16 +6,22 @@
 // Note: if there is nothing to sum, the sum is default to 0.
 
 
+//****MY SOLUTION *****/
+// function positiveSum(arr) {
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++) {
+//       if(arr[i] < 0) {
+//         sum += 0
+//       }
+//       else if(arr[i] >= 0) {
+//         sum += arr[i]
+//       }
+//     }
+//     return sum
+//   }
 
-function positiveSum(arr) {
-    let sum = 0;
-    for(let i = 0; i < arr.length; i++) {
-      if(arr[i] < 0) {
-        sum += 0
-      }
-      else if(arr[i] >= 0) {
-        sum += arr[i]
-      }
-    }
-    return sum
-  }
+
+  function positiveSum(arr) {
+    return arr.reduce((sum,index)=> sum + (index > 0 ? index : 0),0); //array. reduce set paramenters sum, index, and initial value to 0. sum += index if index is greater than 0 else sum += 0
+ }
+ 
